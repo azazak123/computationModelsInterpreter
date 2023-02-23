5a60d8b382d3de96a9b6b1ef0f4bd9a987d5a855
azazak123 <azazaka2002@gmail.com>
2023-02-23 23:39:58 +0200

    build: setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* eslint-env node */
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+  ],
+  plugins: ["@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "tsconfig.eslint.json",
+    tsconfigRootDir: __dirname,
+  },
+  ignorePatterns: ["**/*.js"],
+  root: true,
+};
