2a8b3bee01aed11503e284481375675d6b9a8834
Tkach-Illia <tkachillia00@gmail.com>
2023-02-28 15:38:37 +0200

    feat(header.tsx navlinks.tsx navlinks.scss): add navlinks in header

--- apps/frontend/src/components/header/Header.tsx
+++ apps/frontend/src/components/header/Header.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './Header.scss';
+import NavLinks from "./navLinks/Navlinks";
 
 function Header() : React.ReactElement {
     return (
         <header className="header">
             <h1>My App</h1>
 
+            <NavLinks />
         </header>
     );
 };
---
+++ apps/frontend/src/components/header/navLinks/Navlinks.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./NavLinks.scss"
+function NavLinks() {
+    return (
+        <ul className="nav-links">
+            <li><a href="/apps/front/public">Home</a></li>
+            <li><a href="/apps/front/public">Machines</a></li>
+            <li><a href="/apps/front/public">About</a></li>
+        </ul>
+    );
+}
+
+export default NavLinks;
