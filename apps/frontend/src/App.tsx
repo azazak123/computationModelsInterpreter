ce6058c626b9bfd7381f19f8cea6ae7734eadff8
Tkach-Illia <tkachillia00@gmail.com>
2023-03-01 19:30:29 +0200

    style(/machine): move folder

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/header/Header";
-import Machine from "./components/header/machine/Machine";
+import Machine from "./components/machine/Machine";
 
 function App(): React.ReactElement {
   return (
